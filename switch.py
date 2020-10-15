5c3ef67059cdbee448f90e57592d1e4ed9c15004
Roman <ego-z@mail.ru>
2020-10-15 16:19:58 +0500

    worktest = OK

--- switch.py
+++ switch.py
@@ -1,4 +1,4 @@
-def switch(case_key=None, cases:dict={}):
+def switch(case_key=None, case:dict={}):
     if case.get("default") == None:
             print(f'Case key: "{case_key}" NOT found! and "default" key NOT exist')
             return
@@ -10,30 +10,28 @@ def switch(case_key=None, cases:dict={}):
 NO MORE if - elif - elif - ... - else !!! Just use switch case !!!
 "HOW TO USE: "
 
-
-case_key = 2
+case_key = 1
 
 switch( case_key,
-        cases:dict={
-               0:lambda:(
-                    print(" i am 'zero[0]' case"),
-                    print(" i am 'zero[1]' case"),
-                    print(" i am 'zero[2]' case"),
-                    ),
-               1:lambda:(
-                    print(" i am '1-th' case"),
-                    ),
-               2:lambda:(
-                    print(" i am '2-th' case"),
-                    ),
-             "q":lambda:(
-                    print(" i am 'q' case"),
-                    ),
-       "default":lambda key:(
-                    print(f"Case key: '{key}' not found! i am Default "),
-                    )
-               }
-
+        case = {
+                   0:lambda:(
+                        print(" i am 'zero[0]' case"),
+                        print(" i am 'zero[1]' case"),
+                        print(" i am 'zero[2]' case"),
+                        ),
+                   1:lambda:(
+                        print(" i am '1-th' case"),
+                        ),
+                   2:lambda:(
+                        print(" i am '2-th' case"),
+                        ),
+                 "q":lambda:(
+                        print(" i am 'q' case"),
+                        ),
+           "default":lambda key:(
+                        print(f"Case key: '{key}' not found! i am Default "),
+                        )
+                }
         )
         
 """
