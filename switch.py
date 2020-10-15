9dfa5245cfdba1d0ba071c249869674e4023216d
Roman <ego-z@mail.ru>
2020-10-15 15:39:45 +0500

    add switch case lib

---
+++ switch.py
@@ -0,0 +1,39 @@
+def switch(case_key=None, cases:dict={}):
+    if case.get("default") == None:
+            print(f'Case key: "{case_key}" NOT found! and "default" key NOT exist')
+            return
+    if case.get(case_key) == None:
+            case["default"](case_key)
+    else:
+        case[case_key]()
+"""
+NO MORE if - elif - elif - ... - else !!! Just use switch case !!!
+"HOW TO USE: "
+
+
+case_key = 2
+
+switch( case_key,
+        cases:dict={
+               0:lambda:(
+                    print(" i am 'zero[0]' case"),
+                    print(" i am 'zero[1]' case"),
+                    print(" i am 'zero[2]' case"),
+                    ),
+               1:lambda:(
+                    print(" i am '1-th' case"),
+                    ),
+               2:lambda:(
+                    print(" i am '2-th' case"),
+                    ),
+             "q":lambda:(
+                    print(" i am 'q' case"),
+                    ),
+       "default":lambda key:(
+                    print(f"Case key: '{key}' not found! i am Default "),
+                    )
+               }
+
+        )
+        
+"""
